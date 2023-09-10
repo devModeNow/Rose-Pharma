8ae2bc9229e4253b1ef38bc670154437d41a6937
devModeNow <ronaldfernando.dev@gmail.com>
2023-09-11 03:07:53 +0800

    update db

--- pharma.sql
+++ pharma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 15, 2023 at 10:49 AM
+-- Generation Time: Sep 10, 2023 at 09:03 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -54,8 +54,20 @@ INSERT INTO `tbl_login` (`id`, `username`, `password`, `user_type_id`, `status_i
 CREATE TABLE `tbl_pharma_categories` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `status_id` int(11) NOT NULL
+  `status_id` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pharma_categories`
+--
+
+INSERT INTO `tbl_pharma_categories` (`id`, `name`, `status_id`) VALUES
+(79, 'Analgesics', 1),
+(80, 'Antipyrectics', 1),
+(81, 'Vitamins', 1),
+(82, 'Antibiotics', 1),
+(83, 'Antiseptics', 1),
+(86, 'Mood stabilizers', 1);
 
 -- --------------------------------------------------------
 
@@ -67,18 +79,28 @@ CREATE TABLE `tbl_pharma_products` (
   `id` int(11) NOT NULL,
   `code` varchar(255) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `description` text NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
   `price` float NOT NULL,
   `category_id` int(100) NOT NULL,
   `type_id` int(50) NOT NULL,
-  `unit_id` int(50) NOT NULL,
+  `unit_id` int(50) DEFAULT NULL,
   `quantity` int(11) NOT NULL,
+  `prescription` tinyint(1) DEFAULT 0,
   `status_id` int(11) NOT NULL DEFAULT 1,
   `image` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pharma_products`
+--
+
+INSERT INTO `tbl_pharma_products` (`id`, `code`, `name`, `description`, `price`, `category_id`, `type_id`, `unit_id`, `quantity`, `prescription`, `status_id`, `image`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, '0001', 'Paracetamol', 'Paracetamol', 10, 80, 6, NULL, 1000, 1, 1, '', '2023-09-10 18:44:59', NULL, NULL),
+(2, '0002', 'biogesic', 'biogesic', 8, 80, 6, NULL, 2000, 0, 1, '', '2023-09-10 18:52:56', NULL, NULL),
+(3, '0003', 'testing', 'liquidski', 10, 79, 8, NULL, 200, 1, 1, '', '2023-09-10 19:00:16', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -89,8 +111,19 @@ CREATE TABLE `tbl_pharma_products` (
 CREATE TABLE `tbl_pharma_types` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `status_id` int(11) NOT NULL
+  `status_id` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pharma_types`
+--
+
+INSERT INTO `tbl_pharma_types` (`id`, `name`, `status_id`) VALUES
+(5, 'Drops', 1),
+(6, 'Tablet', 1),
+(7, 'Inhaler', 1),
+(8, 'Liquid', 1),
+(9, 'Capsule', 1);
 
 -- --------------------------------------------------------
 
@@ -131,6 +164,13 @@ CREATE TABLE `tbl_userprofile` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_userprofile`
+--
+
+INSERT INTO `tbl_userprofile` (`id`, `user_id`, `firstname`, `lastname`, `profile_image`, `position_id`, `created_at`, `updated_at`) VALUES
+(1, '1', 'Cristhoper', 'Ferrer', '', 1, '2023-08-22 07:49:36', NULL);
 
 -- --------------------------------------------------------
 
@@ -210,19 +250,19 @@ ALTER TABLE `tbl_login`
 -- AUTO_INCREMENT for table `tbl_pharma_categories`
 --
 ALTER TABLE `tbl_pharma_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `tbl_pharma_products`
 --
 ALTER TABLE `tbl_pharma_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_pharma_types`
 --
 ALTER TABLE `tbl_pharma_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_pharma_units`
@@ -240,7 +280,7 @@ ALTER TABLE `tbl_statuses`
 -- AUTO_INCREMENT for table `tbl_userprofile`
 --
 ALTER TABLE `tbl_userprofile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_usertype`
